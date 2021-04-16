98dc7f759af3f2448928e63d3b89ca1b763c7939
yoshi <yoshiyuki@odysseyenergysolutions.com>
2021-04-15 22:59:52 -0600

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './profile/summary/summary.component';
+import { AboutComponent } from './profile/about/about.component';
+import { ContactComponent } from './profile/contact/contact.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RestInterceptor } from './global/rest-interceptor';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    AboutComponent,
+    ContactComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RestInterceptor,
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
