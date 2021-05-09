f46196e0215a77b5f544a86da7b8d8419a94ea25
yoshi <yoshiyuki@odysseyenergysolutions.com>
2021-05-09 13:00:53 -0600

    changed backend uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   name: 'Portfolio',
   production: true,
   api: {
-    server: 'https://yoshiyukitakahashi.com',
-    url: 'api',
-    version: 'v1.0',
+    server: 'https://yoshiyukitakahashi.com/wp',
+    url: 'wp-json/wp',
+    version: 'v2',
   },
   gTag: {
     id: 'G-4ZZQHKCRW0'
